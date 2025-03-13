1c1057036706d3517394d6735744d555e5882576
quanglong29074 <quanglong29074@gmail.com>
2025-03-13 15:47:41 +0700

    fix error

--- components/Miner/MiningControls.tsx
+++ components/Miner/MiningControls.tsx
@@ -2,12 +2,9 @@ import React, { useState, useEffect } from "react";
 import {TouchableOpacity, Text, ToastAndroid, TextStyle} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { stylesMiner } from "@/app/(tabs)/styles/StylesMiner";
-import AsdMiningRN from "asd-mining-rn";
 
 const MiningControls = ({ isMining, toggleMining } : any) => {
   const [minerLicense, setMinerLicense] = useState(null);
-  const [hashRate, setHashRate] = useState(0)
-  const miner = new AsdMiningRN('784f49cc5411df749e542ae938cb59e66bc0000019ce102474ee5fd82bc0dd30', 'https://be.asdscan.ai');
   useEffect(() => {
     const getMinerLicense = async () => {
       try {
@@ -21,16 +18,13 @@ const MiningControls = ({ isMining, toggleMining } : any) => {
       }
     };
 
-    console.log('hashRate', hashRate)
 
     getMinerLicense();
-  }, [hashRate]);
+  }, []);
 
   const handlePress = async () => {
     if (minerLicense) {
       toggleMining();
-      const hashRate = await miner.calculateHashRate(5000)
-      setHashRate(hashRate)
     } else {
       ToastAndroid.show("Please configure minerLicense first!", ToastAndroid.SHORT);
     }
