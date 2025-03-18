0d2e265b4c79f0f004c5863d27cea213fe21b0cd
quanglong29074 <quanglong29074@gmail.com>
2025-03-18 12:26:08 +0700

    fix error

--- hooks/useMinerHashRate.ts
+++ hooks/useMinerHashRate.ts
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {useMinerId} from "@/hooks/useMinerId";
+import {AsdApi} from "@/lib/api/service/asdApi";
+import {ToastAndroid} from "react-native";
 
 export const useMinerHashRate = () => {
   const [minerHashRate, setMinerHashRate] = useState<string | null>(null);
-
+const minerId = useMinerId();
   useEffect(() => {
+    if (!minerId) return ToastAndroid.show("Lỗi khi lấy dữ liệu thợ đào!", ToastAndroid.SHORT);
     const fetchMinerLicense = async () => {
       try {
-        const storedData = await AsyncStorage.getItem("minerConfig");
-        if (storedData) {
-          const parsedData = JSON.parse(storedData);
-          setMinerHashRate(parsedData.hashRate || null);
-        }
+      const data =  AsdApi.getMiner(Number(minerId));
       } catch (error) {
         console.error("Lỗi khi lấy minerLicense:", error);
       }
